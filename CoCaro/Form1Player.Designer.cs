2772d9230bf1683f2abd975feae1b3a7a772b7e9
Trần Minh Quang <tranquang22042002@gmail.com>
2022-11-01 00:27:54 +0700

    Initial commit

---
+++ CoCaro/Form1Player.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace CoCaro
+{
+    partial class Form1Player
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPlayer = new System.Windows.Forms.TextBox();
+            this.cbbLevel = new System.Windows.Forms.ComboBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên người chơi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mời nhập thông tin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chọn cấp độ";
+            // 
+            // txtPlayer
+            // 
+            this.txtPlayer.Location = new System.Drawing.Point(165, 80);
+            this.txtPlayer.Name = "txtPlayer";
+            this.txtPlayer.Size = new System.Drawing.Size(314, 26);
+            this.txtPlayer.TabIndex = 3;
+            // 
+            // cbbLevel
+            // 
+            this.cbbLevel.FormattingEnabled = true;
+            this.cbbLevel.Items.AddRange(new object[] {
+            "Level 1",
+            "Level 2",
+            "Level 3",
+            "Level 4",
+            "Level 5"});
+            this.cbbLevel.Location = new System.Drawing.Point(165, 128);
+            this.cbbLevel.Name = "cbbLevel";
+            this.cbbLevel.Size = new System.Drawing.Size(161, 28);
+            this.cbbLevel.TabIndex = 4;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(203, 182);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 33);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Chơi";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // Form1Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 227);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.cbbLevel);
+            this.Controls.Add(this.txtPlayer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1Player";
+            this.Text = "Form1Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPlayer;
+        private System.Windows.Forms.ComboBox cbbLevel;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
---
+++ CoCaro/FormStart.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace CoCaro
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormStart));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOnePlayer = new System.Windows.Forms.Button();
+            this.btnTwoPlayer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(276, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Caro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(205, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(355, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vui lòng chọn chế độ chơi";
+            // 
+            // btnOnePlayer
+            // 
+            this.btnOnePlayer.Location = new System.Drawing.Point(212, 302);
+            this.btnOnePlayer.Name = "btnOnePlayer";
+            this.btnOnePlayer.Size = new System.Drawing.Size(142, 39);
+            this.btnOnePlayer.TabIndex = 2;
+            this.btnOnePlayer.Text = "Chơi với máy";
+            this.btnOnePlayer.UseVisualStyleBackColor = true;
+            this.btnOnePlayer.Click += new System.EventHandler(this.btnOnePlayer_Click);
+            // 
+            // btnTwoPlayer
+            // 
+            this.btnTwoPlayer.Location = new System.Drawing.Point(418, 302);
+            this.btnTwoPlayer.Name = "btnTwoPlayer";
+            this.btnTwoPlayer.Size = new System.Drawing.Size(142, 39);
+            this.btnTwoPlayer.TabIndex = 3;
+            this.btnTwoPlayer.Text = "Chơi 2 người";
+            this.btnTwoPlayer.UseVisualStyleBackColor = true;
+            this.btnTwoPlayer.Click += new System.EventHandler(this.btnTwoPlayer_Click);
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTwoPlayer);
+            this.Controls.Add(this.btnOnePlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormStart";
+            this.Text = "Start";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOnePlayer;
+        private System.Windows.Forms.Button btnTwoPlayer;
+    }
+}
